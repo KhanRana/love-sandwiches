ec508a9b8a0b920f7d53dcceacd8c98477d08e80
KhanRana <rana_khan@outlook.com>
2022-11-17 23:21:54 +0000

    calculate surplus data

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
+# from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -21,12 +21,9 @@ def get_sales_data():
     while True:
         print("Please enter the sales data from the last market.")
         print("Data should be six numbers, seperated by commas.")
-        print("For example 1,2,3,4,5,6\n")
-        
+        print("For example 1,2,3,4,5,6\n")     
         data_str = input("Enter your data here: ")
-        
-        sales_data = data_str.split(',')
-        
+        sales_data = data_str.split(',')    
         if validate_data(sales_data):
             print("Valid data!")
             break
@@ -63,6 +60,15 @@ def update_sales_worksheet(data):
     print("Sales worksheet has been updated successfully.\n")
 
 
+def user_sales_data():
+    """
+    get sales data from the sheet that user entered
+    """
+    new_sales = SHEET.worksheet("sales").get_values()
+    sales_row = new_sales[-1]
+    return sales_row
+
+
 def calculate_surplus_data(sales_row):
     """
     Compare stock with sales and calculate surplus
@@ -71,6 +77,14 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = (stock[-1])
+    stock_data = [int(stock) for stock in stock_row]
+
+    surplus_data = []
+    for stock, sales in zip(stock_data, sales_row):
+        surplus = stock - int(sales)
+        surplus_data.append(surplus)
+    
+    return surplus_data
 
 
 def main():
@@ -79,8 +93,9 @@ def main():
     """
     user_data = get_sales_data()
     update_sales_worksheet(user_data)
-    calculate_surplus_data(user_data)
+    sales_data = user_sales_data()
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
 
 
 main()
-
